68584d296170528f346b501398adf4aeebba6f2b
Matt Ray <matthew.ray@va.gov>
2022-07-27 09:13:26 -0500

    Added simple banner component

---
+++ app/javascript/bundles/TestApp/components/Banner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Banner = () => (
+    <div style={{backgroundColor: "rgba(255, 0, 0, 0.5)", padding: "5px"}}>
+        <p>We are having some technical issues. You can continue to work but you might have degradaded services.</p>
+    </div>
+)
+
+export default Banner;
--- app/javascript/bundles/TestApp/components/TestApp.jsx
+++ app/javascript/bundles/TestApp/components/TestApp.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Banner from "./Banner";
 
 const TestApp = () => (
+    <React.Fragment>
+    <Banner/>
+    <h1>Test App</h1>
     <div>
         Welcome to the test app
     </div>
+    </React.Fragment>
 )
 
 export default TestApp;
