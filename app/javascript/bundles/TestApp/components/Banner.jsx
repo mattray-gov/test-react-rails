31cf58ca0eab30cd3592831027a23c17536a48d3
Matt Ray <matthew.ray@va.gov>
2022-07-27 09:23:50 -0500

    fixed spelling

--- app/javascript/bundles/TestApp/components/Banner.jsx
+++ app/javascript/bundles/TestApp/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = () => (
     <div style={{backgroundColor: "rgba(255, 0, 0, 0.5)", padding: "5px"}}>
-        <p>We are having some technical issues. You can continue to work but you might have degradaded services.</p>
+        <p>We are having some technical issues. You can continue to work but you might have degraded services.</p>
     </div>
 )
 
